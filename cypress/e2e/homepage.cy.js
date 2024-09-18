a6004e04a0f65751e85111b2a70f56c575a66dc6
IgnacioTejeraPicossi <ignacio.tejera@outlook.com>
2024-09-18 15:11:13 +0200

    Url change 2

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,7 +1,7 @@
 describe('Hjemmeside tester', () => {
   it('Verifiser at hjemmesiden laster korrekt', () => {
     cy.visit('/');  // Dette tilsvarer å besøke http://localhost:3000/no
-    cy.contains('CICERO').should('be.visible');  // Sjekk at teksten "Velkommen" er synlig
+    cy.contains('CICERO').should('be.visible');  // Sjekk at teksten "CICERO" er synlig
   });
 });
 
