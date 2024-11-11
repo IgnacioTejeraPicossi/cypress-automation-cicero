52be4c80ca3cc82687b69ccc5653fc7cc59046f9
IgnacioTejeraPicossi <ignacio.tejera@outlook.com>
2024-11-11 09:28:39 +0100

    Add Topics testu

--- cypress/e2e/menuNor.cy.js
+++ cypress/e2e/menuNor.cy.js
@@ -69,52 +69,20 @@ describe('Test NO menu', () => {
     
        
     });
-  });
-
-  describe('Test EN menu', () => {
-    it('Test EN menu', () => {
-
+    it('Test NO under menu Tema', () => {
       cy.visit('/');
-      cy.visit('http://localhost:3000/en');
-      cy.contains('CICERO').should('be.visible');  // Sjekk at teksten "CICERO" er synlig
-      cy.contains('Events').should('be.visible'); 
-      cy.contains('Newsletter').should('be.visible'); 
-      cy.contains('About').should('be.visible'); 
-      cy.contains('Projects').should('be.visible'); 
-      cy.contains('Research groups').should('be.visible'); 
-      cy.contains('Topics').should('be.visible'); 
-      cy.contains('Employees').should('be.visible'); 
-      cy.contains('Articles').should('be.visible'); 
-      cy.contains('Publications').should('be.visible'); 
-                  
-    });
-    it('Test EN under menu employees', () => {
-      cy.visit('/');
-      cy.visit('http://localhost:3000/en/employees');
-      cy.contains('employees').should('be.visible');~
+      cy.visit('http://localhost:3000/no/tema');
+      cy.contains('Tema').should('be.visible');~
 
        /* ==== Test meny opsjon ==== */
        cy.visit('/');
-       //div[normalize-space()='Ansatte']
-       cy.visit('http://localhost:3000/en/employees');
+       cy.visit('http://localhost:3000/no/tema');
        cy.get('a:nth-child(2) .styles_title__Ha2Nn').click();
-       cy.contains('employees');
-      
-    });
-    it('Test EN under menu Projects', () => {
-      cy.visit('/');
-      cy.visit('http://localhost:3000/en/Projects');
-      cy.contains('Ansatte').should('be.visible');~
-
-       /* ==== Test meny opsjon ==== */
-       cy.visit('/');
-       cy.visit('http://localhost:3000/no/Projects');
-       cy.get('a:nth-child(2) .styles_title__Ha2Nn').click();
-       cy.contains('Projects');
-      
-       
+       cy.contains('Tema');
+           
     });
   });
 
+  
  // cy.visit('http://localhost:3000/no');
 
